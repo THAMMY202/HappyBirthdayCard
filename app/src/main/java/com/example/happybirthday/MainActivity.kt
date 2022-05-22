d78ec629e89f86ef18e99794f9f1c15e8b061421
Thamsanqa Shabalala <thammy202@gmail.com>
2022-05-22 12:27:14 +0200

    Playing birthday song

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.happybirthday
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playBirthDayWishSong()
+    }
+
+    private fun playBirthDayWishSong(){
+        var resId = resources.getIdentifier(R.raw.birthday_song.toString(),"raw", this?.packageName)
+
+        val mediaPlayer = MediaPlayer.create(this, resId)
+        mediaPlayer.start()
     }
 }
